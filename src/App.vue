39d3a7839bd857bd2b990aa2eab9f87e184bef8b
Camilo García <gcgarciab@unal.edu.co>
2021-12-02 18:46:59 -0500

    Timer v1

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,71 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <div class="timer">
+      <div class="board">
+        <p>{{ formatNumber(minutes) }}:{{ formatNumber(seconds) }}:{{ formatNumber(miliseconds) }}</p>
+      </div>
+
+      <div class="actions">
+        <button @click="play()" :disabled="isRunning">Play</button>
+        <button @click="pause()">Pause</button>
+        <button @click="reset()">Reset</button>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  
+  data() {
+    return {
+      minutes: 0,
+      seconds: 0,
+      miliseconds: 0,
+      isRunning: false,
+      interval: null,
+    }
+  },
+
+  methods: {
+    formatNumber(number) {
+      if (number < 10){
+        return '0' + number;
+      } else {
+        return number;
+      }
+    },
+
+    play() {
+      this.isRunning = true;
+
+      this.interval = setInterval(() => {
+        this.miliseconds++;
+
+        if (this.miliseconds === 100){
+          this.miliseconds = 0;
+          this.seconds++;
+        }
+
+        if (this.seconds === 60){
+          this.seconds = 0;
+          this.minutes++;
+        }
+      }, 10);
+    },
+
+    pause() {
+      this.isRunning = false;
+      clearInterval(this.interval);
+    },
+
+    reset() {
+      this.pause();
+      this.minutes = this.seconds = this.miliseconds = 0;
+    }
+  },
 }
 </script>
 
