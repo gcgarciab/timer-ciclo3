479396dbbf8e5133f8cf86f0454dc6a3ed7c73d6
Camilo García <gcgarciab@unal.edu.co>
2021-12-03 17:38:15 -0500

    Timer with styles

--- src/App.vue
+++ src/App.vue
@@ -70,12 +70,86 @@ export default {
 </script>
 
 <style>
+body {
+  margin: 0;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+.container {
+  height: 100vh;
+  background-color: #018C8C;
+  display: flex;
+	flex-direction: row;
+	flex-wrap: nowrap;
+	justify-content: center;
+	align-items: center;
+  align-content: center;
+}
+
+.timer {
+  background-color: #fff;
+  padding: 10% 15%;
+  width: 80%;
+  height: 80vh;
+  box-sizing: border-box;
+  border: 2px solid #000;
+  display: flex;
+	flex-direction: column;
+	flex-wrap: nowrap;
+	justify-content: center;
+	align-items: stretch;
+	align-content: center;
+}
+
+.board {
+  background-color: #4D4C4C;
+  color: #fff;
+  height: 60%;
+  display: flex;
+	flex-direction: row;
+	flex-wrap: nowrap;
+	justify-content: center;
+	align-items: center;
+  align-content: center;
+  border: 2px solid #000;
+}
+
+p {
+  margin: 0;
+  font-size: 60px;
+}
+
+.actions {
+  background-color: #DB143C;
+  padding: 40px;
+  display: flex;
+	flex-direction: row;
+	flex-wrap: nowrap;
+	justify-content: space-between;
+	align-items: center;
+	align-content: center;
+  border: 2px solid #000;
+  border-top: 0;
+}
+
+.actions button {
+  border-radius: none;
+  border: 2px solid #000;
+  font-size: 20px;
+  font-weight: bold;
+  padding: 8px 15px;
+  color: #000;
+  cursor: pointer;
+}
+
+button:disabled {
+  cursor: no-drop;
 }
 </style>
